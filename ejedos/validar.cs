bc97970c50b82c014f3ece734fd998afa7bda9ee
ARVIOJ <ol439950@uaeh.edu.mx>
2023-04-04 02:21:00 -0600

    primer commit

---
+++ ejedos/validar.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+    namespace ejedos
+    {
+        class validar
+        {
+            public static void SoloNumeros(KeyPressEventArgs pE)
+            {
+                //Para obligar a que sólo se introduzcan números
+                if (Char.IsDigit(pE.KeyChar))
+                {
+                    pE.Handled = false;
+                }
+                else if (Char.IsControl(pE.KeyChar)) //permitir teclas de control como retroceso
+                {
+                    pE.Handled = false;
+                }
+                else if (Char.IsPunctuation(pE.KeyChar))//permitir teclas de puntuacion
+                {
+                    pE.Handled = false;
+                }
+                else //el resto de teclas pulsadas se desactivan
+                {
+                    pE.Handled = true;
+                }
+
+                }
+
+            public static void SoloLetras(KeyPressEventArgs pE)
+            {
+                //Para obligar a que sólo se introduzcan letras
+                    if (Char.IsLetter(pE.KeyChar))
+                    {
+                    pE.Handled = false;
+                }
+                else if (Char.IsControl(pE.KeyChar))//permitir teclas de control como retroceso
+                {
+                    pE.Handled = false;
+                }
+                else if (Char.IsSeparator(pE.KeyChar))//permite tecla de espacio
+                {
+                    pE.Handled = false;
+                }
+                else //el resto de teclas pulsadas se desactivan
+                {
+                    pE.Handled = true;
+                }
+            }
+
+
+        }
+
+        }
